6295979bde7fc849ab36ecb86b42dbb04c44c819
mak7on22 <mur-mur-0998@mail.ru>
2023-11-25 18:59:47 +0600

    Added a class of companies

---
+++ WebMoney/Models/Company.cs
@@ -0,0 +1,9 @@
+﻿namespace WebMoney.Models
+{
+    public class Company
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public int? Money { get; set; }
+    }
+}
