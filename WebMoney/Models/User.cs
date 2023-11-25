41ddc3ff09a47bbe82919c5df34ceeff37bf52b4
mak7on22 <mur-mur-0998@mail.ru>
2023-11-25 19:00:33 +0600

    Added a users  class

---
+++ WebMoney/Models/User.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using WebMoney.ViewModels;
+
+namespace WebMoney.Models
+{
+	public class User : IdentityUser<int>
+	{
+		public int? Balance { get; set; }
+		public int? Indificator { get; set; }
+		public string? RoleName { get; set; }
+
+		public ICollection<TransferHistory> TransferHistories { get; set; }
+    }
+}
