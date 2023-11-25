31e0011b2bc4e65b705a44c8b220a49361ec9162
mak7on22 <mur-mur-0998@mail.ru>
2023-11-25 19:02:28 +0600

    Added administration services

---
+++ WebMoney/Servises/AdminInitial.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Identity;
+using WebMoney.Models;
+
+namespace WebMoney.Servises
+{
+    public class AdminInitial
+    {
+        public static async Task SeedAdminUser(RoleManager<IdentityRole<int>> roleManager, UserManager<User> userManager)
+        {
+            try
+            {
+                string adminEmail = "admin@mail.ru";
+                string adminPassword = "123!A123";
+                string adminLogin = "AdminaSosok22";
+                var roles = new[] { "Admin", "Member" };
+
+                foreach (var roleName in roles)
+                {
+                    if (await roleManager.FindByNameAsync(roleName) == null)
+                        await roleManager.CreateAsync(new IdentityRole<int>(roleName));
+                }
+
+                if (await userManager.FindByEmailAsync(adminEmail) == null)
+                {
+                    User admin = new() { Email = adminEmail, UserName = adminLogin, RoleName = "Admin", Indificator = 000000, Balance = 999999 };
+                    var result = await userManager.CreateAsync(admin, adminPassword);
+
+                    if (result.Succeeded)
+                        await userManager.AddToRoleAsync(admin, "Admin");
+                    else
+                        throw new Exception($"Failed to create admin user. Errors: {string.Join(", ", result.Errors)}");
+                }
+            }
+            catch (Exception ex) { Console.Error.WriteLine($"An error occurred during admin user seeding: {ex.Message}"); }
+        }
+    }
+}
---
+++ WebMoney/Servises/UserService.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Caching.Memory;
+using WebMoney.Models;
+
+namespace WebMoney.Servises
+{
+    public class UserService
+    {
+        private MoneyContext context;
+        private IMemoryCache cache;
+
+        public UserService(MoneyContext context, IMemoryCache cache)
+        {
+            this.cache = cache;
+            this.context = context;
+        }
+        public async Task<IEnumerable<User>> GetUsers() => await context.Users.ToListAsync();
+
+        public async Task AddUSer(User user)
+        {
+            await context.Users.AddAsync(user);
+            int s = await context.SaveChangesAsync();
+            if (s > 0)
+                cache.Set(user.Id, user, new MemoryCacheEntryOptions { AbsoluteExpirationRelativeToNow = TimeSpan.FromMinutes(5) });
+        }
+        public async Task<User> GetUser(int id)
+        {
+            User user = null;
+            if (!cache.TryGetValue(id, out user))
+            {
+                user = await context.Users.FirstOrDefaultAsync(u => u.Id == id);
+                if (user != null)
+                    cache.Set(user.Id, user, new MemoryCacheEntryOptions().SetAbsoluteExpiration(TimeSpan.FromMinutes(5)));
+            }
+            return user;
+        }
+    }
+}
