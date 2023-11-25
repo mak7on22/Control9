2e27cf5281a6f3b53ce1f6e40305cb900bcd9042
mak7on22 <mur-mur-0998@mail.ru>
2023-11-25 19:00:08 +0600

    Added a translation class

---
+++ WebMoney/Models/Replenish.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebMoney.Models
+{
+    public class Replenish
+    {
+        [NotMapped]
+        public string? AdditionalProperty { get; set; }
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id
+        {
+            get
+            {
+                return _id == 2 ? GenerateRandomId() : _id;
+            }
+            set { _id = value; }
+        }
+        private int _id;
+
+        public string? IndificatorOrPhone { get; set; }
+        public int? Amount { get; set; }
+        public int UserId { get; set; }
+        public DateTime Time { get; set; }
+        public User User { get; set; }
+
+        private int GenerateRandomId()
+        {
+            Random random = new Random();
+            return random.Next(10000,9999999);
+        }
+    }
+
+
+}
