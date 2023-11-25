566ce3658abdaed0c6dcbbb7b171c38166240b6d
mak7on22 <mur-mur-0998@mail.ru>
2023-11-25 19:00:54 +0600

    Added a translation history class

---
+++ WebMoney/Models/TransferHistory.cs
@@ -0,0 +1,23 @@
+﻿namespace WebMoney.Models
+{
+    public class TransferHistory
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int ReplenishId { get; set; }
+        public DateTime TransactionDate { get; set; }
+        public TransactionType Type { get; set; }
+        public int Amount { get; set; }
+        public int? ReceiverId { get; set; }
+        public User User { get; set; }
+        public Replenish Replenish { get; set; }
+        public User Receiver { get; set; }
+    }
+
+    public enum TransactionType
+{
+    Replenish,
+    Transfer,
+    Withdraw
+}
+}
